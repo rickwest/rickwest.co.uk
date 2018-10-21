0c4b4a08b8315a8cdc7c270eb40a3ca40ceb5ee7
rick <rickwestdev@gmail.com>
2018-10-21 13:18:46 +0100

    delete newsletter page, link to tiny letter landing page instead

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -14,7 +14,7 @@
                     <a class="nav-link" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/newsletter">Newsletter</a>
+                    <a class="nav-link" href="https://tinyletter.com/rickwest">Newsletter</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact</a>
