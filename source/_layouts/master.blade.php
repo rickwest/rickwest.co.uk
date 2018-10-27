48af3b167e1ec4c9c0c5b855433469d8614b5f5c
rick <rickwestdev@gmail.com>
2018-10-27 23:09:53 +0100

    add basic seo title, description, image meta

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,9 +5,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <meta name="description" content="Rick West | Dad. Husband. Developer. Heating Engineer. Human."/>
+        <title>{{ $page->title ? $page->title . ' - Rick West' : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}</title>
+        <!-- Search Engine -->
+        <meta name="description" content="{{ $page->excerpt ? $page->excerpt : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta name="image" content="{{ $page->image ? $page->baseUrl . $page->image : $page->baseUrl . '/assets/images/backgrounds/home-bg.jpg' }}">
+        <!-- Schema.org for Google -->
+        <meta itemprop="name" content="{{ $page->title ? $page->title : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta itemprop="description" content="{{ $page->excerpt ? $page->excerpt : 'Rick West | Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta itemprop="image" content="{{ $page->image ? $page->baseUrl . $page->image : $page->baseUrl . '/assets/images/backgrounds/home-bg.jpg' }}">
+        <!-- Twitter -->
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="{{ $page->title ? $page->title : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta name="twitter:description" content="{{ $page->excerpt ? $page->excerpt : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta name="twitter:site" content="@rick_west8">
+        <meta name="twitter:creator" content="@rick_west8">
+        <meta name="twitter:image:src" content="{{ $page->image ? $page->baseUrl . $page->image : $page->baseUrl . '/assets/images/backgrounds/home-bg.jpg' }}">
+        <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+        <meta name="og:title" content="{{ $page->title ? $page->title : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta name="og:description" content="{{ $page->excerpt ? $page->excerpt : 'Rick West | Dad. Husband. Developer. Heating Engineer. Human.' }}">
+        <meta name="og:image" content="{{ $page->image ? $page->baseUrl . $page->image : $page->baseUrl . '/assets/images/backgrounds/home-bg.jpg' }}">
+        <meta name="og:url" content="{{ $page->getUrl() }}">
+        <meta name="og:site_name" content="{{ $page->siteName }}">
+        <meta name="og:locale" content="en_GB">
+        <meta name="og:type" content="website">
 
-        <title>Rick West | Dad. Husband. Developer. Heating Engineer. Human.</title>
+        <link rel="canonical" href="{{ $page->getUrl() }}" />
 
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
