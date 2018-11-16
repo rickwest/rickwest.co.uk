a8769eda554ad2fc0d336c0e5e47f49e282c5b45
rick <rickwestdev@gmail.com>
2018-11-16 23:14:12 +0000

    some additional config and tidy up for prod

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-    // Replace with the baseUrl of your site. For example, https://jigsaw-clean-blog.netlify.com
-    'baseUrl' => 'https://rickwest.netlify.com',
+    'baseUrl' => 'https://www.rickwest.co.uk',
     'production' => true,
 
 
@@ -17,15 +16,12 @@ return [
     // Number of collection items to show per page
     'perPage' => 10,
 
-    // The email address to send the https://formspree.io/ contact form submissions to
-    'email' => '',
-
     // The name of the site. This is used in the nav and footer
     'siteName' => 'rickwest.co.uk',
     'siteDescription' => '',
     'siteAuthor' => 'rickwestdev@gmail.com (Rick West)',
 
-    // Social media links/icons that are used in the footer, add as many as you like!
+    // Social media links/icons that are used in the footer.
     'socials' => [
         'twitter' => [
             'link' => 'https://twitter.com/rick_west8',
@@ -39,11 +35,23 @@ return [
             'link' => 'https://github.com/rickwest',
             'icon' => 'fab fa-github',
         ],
+        'linkedin' => [
+            'link' => 'https://www.linkedin.com/in/rickwest8/',
+            'icon' => 'fab fa-linkedin',
+        ],
+        'tinyletter' => [
+            'link' => 'https://tinyletter.com/rickwest',
+            'icon' => 'fas fa-heart',
+        ],
         'email' => [
             'link' => 'mailto:rickwestdev@gmail.com',
             'icon' => 'fas fa-envelope',
             'target' => '_self',
         ],
+        'rss' => [
+            'link' => 'https://www.rickwest.co.uk/feed.xml',
+            'icon' => 'fas fa-rss',
+        ]
     ],
 
     'newsletter' => 'https://tinyletter.com/rickwest'
