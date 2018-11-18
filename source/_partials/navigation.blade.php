ea44475971372c4a365c44770885ef319991f48e
rick <rickwestdev@gmail.com>
2018-11-18 23:51:43 +0000

    change home link to 'blog'

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link" href="/">Blog</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/about">About</a>
